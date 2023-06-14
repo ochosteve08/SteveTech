e2f2ae8902ad4f80452d3dc93ce712ea595a1a39
ochosteve <ochosteve08@gmail.com>
2023-06-14 14:51:47 +0100

    setup mongo database and connect project to it, create model for user and note

---
+++ backend/models/user.models.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required:true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    roles: [
+      {
+        type: String,
+        default: "Employee",
+      },
+    ],
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const UserModel = mongoose.model('User',userSchema);
+module.exports = UserModel;
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const app= express();
-const PORT= process.env.PORT ||3500;
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.APP_PORT || 3000;
+const path = require("path");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbCon");
 
 //thirdparty middleware
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 
 // custom middleware
-const errorHandler = require('./middleware/errorHandler')
-const {logger} = require('./middleware/logger')
+const errorHandler = require("./middleware/errorHandler");
+const { logEvents, logger } = require("./middleware/logger");
 
+connectDB();
 
 app.use(logger);
-
 
 //in-built middleware
 app.use(express.json());
 
-app.use(cookieParser())
-app.use(cors(corsOptions))
+app.use(cookieParser());
+app.use(cors(corsOptions));
 
 // app.use(
 //   cors({
@@ -30,25 +33,31 @@ app.use(cors(corsOptions))
 //   })
 // );
 
-
-app.use('/', express.static(path.join(__dirname,'public') ));
+app.use("/", express.static(path.join(__dirname, "public")));
 // app.use(express.static('public'))
-app.use('/',require('./routes/root'));
-app.all('*', (req,res)=>{
-    res.status(404)
-    if (req.accepts('html')){
-        res.sendFile(path.join(__dirname,'views','404.html'))
-    }
-    else if (req.accepts('json')){
-        res.json({message: '404 Not Found'})
-    }
-    else {
-        res.type('txt').send('404 Not Found')
-    }
-})
-
-app.use(errorHandler)
-
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT} `)
+app.use("/", require("./routes/root"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
+
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  console.info("connected to mongodb");
+
+  app.listen(PORT, () => {
+    console.log(`server running on ${process.env.APP_HOST}:${PORT} `);
+  });
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log(error);
+    logEvents(`${error.no}:${error.code}\t${error.syscall}\t${error.hostname}`,'mongoErrorLog.log')
+})
