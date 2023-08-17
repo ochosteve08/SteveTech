5be30c21d5e1397332199edd4531fc69d31d0ef3
ochosteve <ochosteve08@gmail.com>
2023-08-17 19:40:09 +0100

    modified logging codebase

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -2,9 +2,11 @@ const {logEvents} = require('./logger')
 
 const errorHandler = (err,req,res,next)=>{
       logEvents(
-       `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
-       "errLog.log"
-     );
+        `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${
+          req.headers.origin || "Origin not provided"
+        }`,
+        "errLog.log"
+      );
      console.log(err.stack);
 
   const status = res.statusCode ? res.statusCode : 500; //server error
--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -1,28 +1,33 @@
-const {format} = require('date-fns')
-const {v4: uuid} = require('uuid')
-const fs = require('fs')
-const fsPromises = require('fs').promises
-const path = require('path')
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
 
-const logEvents = async (message, logFileName)=>{
-    const dateTime = `${format(new Date(),'yyyyMMdd\tHH:mm:ss')}`
-    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+const logEvents = async (message, logFileName) => {
+  const dateTime = format(new Date(), "yyyyMMdd\tHH:mm:ss");
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
-    try{
-        if (!fs.existsSync(path.join(__dirname,'..','logs'))){
-            await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
-        }
-        await fsPromises.appendFile(path.join(__dirname, "..", "logs",logFileName),logItem);
+  try {
+    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
     }
-    catch(error){
-        console.log(error)
-    }
-}
+    await fsPromises.appendFile(
+      path.join(__dirname, "..", "logs", logFileName),
+      logItem
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const logger =(req, res,next)=>{
-    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`,'reqlog.log')
-    console.log(`${req.method} ${req.path}`)
-    next()
-}
+const logger = (req, res, next) => {
+  logEvents(
+    `${req.method}\t${req.url}\t${req.headers.origin || "Origin not provided"}`,
+    "reqlog.log"
+  );
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
-module.exports = {logEvents, logger}
+module.exports = { logEvents, logger };
