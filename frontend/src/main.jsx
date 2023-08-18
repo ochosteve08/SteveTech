a967c2633de81497b29a69e6622042ebbc76f63a
ochosteve <ochosteve08@gmail.com>
2023-08-18 20:37:17 +0100

    refactor and clean out bug on dashboard footer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
