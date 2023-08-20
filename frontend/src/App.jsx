e653904f777b1a3345c4a753b769b4f575422ab3
ochosteve <ochosteve08@gmail.com>
2023-08-20 08:13:57 +0100

    create components and routes for new-edit user and note

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,10 @@ import DashLayout from "./Components/DashLayout";
 import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
 import UsersList from "./features/users/UsersList"
+import EditUser from "./features/users/EditUser";
+import NewUser from "./features/users/NewUser";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
 
 function App() {
   return (
@@ -15,11 +19,17 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-          </Route>
+
           <Route path="users">
             <Route index element={<UsersList />} />
+            <Route path=":id" element={<EditUser />} />
+            <Route path="new" element={<NewUser />} />
+          </Route>
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+            <Route path=":id" element={<EditNote />} />
+            <Route path="new" element={<NewNote />} />
           </Route>
         </Route>
       </Route>
---
+++ frontend/src/features/users/EditUser.jsx
@@ -0,0 +1,10 @@
+
+
+const EditUser = () => {
+     
+  return (
+    <div>EditUser</div>
+  )
+}
+
+export default EditUser
