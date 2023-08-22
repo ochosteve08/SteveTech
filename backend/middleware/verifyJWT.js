edf94248d0867f4c89a0fec00b20da2f308a8337
ochosteve <ochosteve08@gmail.com>
2023-08-22 08:00:41 +0100

    add verifyJWT functionality to API routes

---
+++ backend/middleware/verifyJWT.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+  if (!authHeader || !authHeader?.startsWith("Bearer")) {
+    return res.status(401).json({ message: "unauthorized" });
+  }
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ message: "forbidden" });
+    req.user = decoded.UserInfo.username;
+    req.roles = decoded.UserInfo.roles;
+    next();
+  });
+};
+
+module.exports = verifyJWT;
