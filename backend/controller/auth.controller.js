0f6f026391373e7437ff4c78ef76216771d15641
ochosteve <ochosteve08@gmail.com>
2023-08-23 08:29:50 +0100

    fix auth bugs

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const { userValidation } = require("../validations/");
-const { forbidden } = require("joi");
 
 //  @route POST /auth
 //  @access public -- if access token has expired
@@ -13,11 +12,12 @@ const Login = asyncHandler(async (req, res, next) => {
 
   const foundUser = await UserModel.findOne({ username }).exec();
   if (!foundUser || !foundUser.active) {
-    return res.status(401).json({ message: "unauthorized" });
+    return res.status(401).json({ message: "no user found" });
   }
-
+  console.log("foundUser:", foundUser);
   const match = await bcrypt.compare(password, foundUser.password);
-  if (!match) res.status(401).json({ message: "unauthorized" });
+  if (!match) return res.status(401).json({ message: "credentials mismatch" });
+  console.log("match:", match);
   const accessToken = jwt.sign(
     {
       userInfo: {
@@ -26,7 +26,7 @@ const Login = asyncHandler(async (req, res, next) => {
       },
     },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "600s" }
+    { expiresIn: "30s" }
   );
 
   const refreshToken = jwt.sign(
@@ -35,7 +35,7 @@ const Login = asyncHandler(async (req, res, next) => {
     },
 
     process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: "1d" }
+    { expiresIn: "60s" }
   );
 
   // create secure cookie with refresh token
@@ -43,7 +43,7 @@ const Login = asyncHandler(async (req, res, next) => {
     httpOnly: true,
     secure: true,
     sameSite: "None",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    // maxAge: 60 * 1000,
   });
 
   // send accessToken containing username and roles
@@ -63,7 +63,7 @@ const Refresh = (req, res) => {
     process.env.REFRESH_TOKEN_SECRET,
     asyncHandler(async (err, decoded) => {
       if (err) return res.status(403).json({ message: "forbidden" });
-      foundUser = await UserModel.findOne({ username: decoded.username });
+      const foundUser = await UserModel.findOne({ username: decoded.username });
       if (!foundUser) return res.status(401).json({ message: "unauthorized" });
       const accessToken = jwt.sign(
         {
@@ -73,7 +73,7 @@ const Refresh = (req, res) => {
           },
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "600s" }
+        { expiresIn: "5s" }
       );
       res.json({ accessToken });
     })
@@ -91,7 +91,7 @@ const Logout = (req, res) => {
     secure: true,
     sameSite: "None",
   });
-  res.json({ message: "cookie cleared" });
+  res.json({ message: "Logged out successfully" });
 };
 
 module.exports = {
--- frontend/src/features/users/UsersList.jsx
+++ frontend/src/features/users/UsersList.jsx
@@ -8,10 +8,10 @@ const UsersList = () => {
     isSuccess,
     isError,
     error,
-  } = useGetUsersQuery(undefined,{
-    pollingInterval:60000,
-    refetchOnFocus:true,
-    refetchOnMountOrArgChange:true,
+  } = useGetUsersQuery("usersList", {
+    pollingInterval: 60000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
   });
 
   let content;
