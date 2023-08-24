b82d0298afe274534c20ce05ba70323f052de470
ochosteve <ochosteve08@gmail.com>
2023-08-24 09:01:03 +0100

    refactored editUser component to incorporate normalized data

--- frontend/src/features/users/EditUser.jsx
+++ frontend/src/features/users/EditUser.jsx
@@ -1,37 +1,16 @@
-// import { useParams } from "react-router-dom";
-// import { useGetUsersQuery } from "./UsersApiSlice";
-// import EditUserForm from "./EditUserForm";
-
-// const EditUser = () => {
-//   const { id } = useParams();
-
-//   const { data: users, isFetching } = useGetUsersQuery();
-
-//   const user = users?.entities[id];
-
-//   let content;
-//   if (isFetching && !user) {
-//     content = <p>Loading...</p>;
-//   } else if (user) {
-//     content = <EditUserForm user={user} />;
-//   } else {
-//     content = <p>No user found</p>;
-//   }
-
-//   return content;
-// };
-// export default EditUser;
-
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectUserById } from "./UsersApiSlice";
+import { useGetUsersQuery } from "./UsersApiSlice";
 import EditUserForm from "./EditUserForm";
 
 const EditUser = () => {
   const { id } = useParams();
+  const { user } = useGetUsersQuery("userList", {
+    selectFromResult: ({ data }) => ({
+      user: data?.entities[id],
+    }),
+  });
 
-  const user = useSelector((state) => selectUserById(state, id));
-
+  console.log(user);
   const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>;
 
   return content;
